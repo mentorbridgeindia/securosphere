41bf6b09b100f8e89b86f9f2ec66bbe6fc58c0f8
Jagan6923 <jeyaraman.saravanan23@gmail.com>
2024-12-09 19:31:34 +0530

    navbar added and sentry implemented successfully

--- src/index.tsx
+++ src/index.tsx
@@ -6,8 +6,18 @@ import reportWebVitals from './reportWebVitals';
 import * as Sentry from "@sentry/react";
 
 Sentry.init({
-  dsn: "https://355f968adae109cb3974461d7eca3fad@o4508431058468864.ingest.de.sentry.io/4508431066267728",
-  integrations: [],
+  dsn: "https://553cc39938dcdc71a390d50490bdd693@o4508437918842880.ingest.de.sentry.io/4508438503161936",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration(),
+  ],
+  // Tracing
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
 });
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
