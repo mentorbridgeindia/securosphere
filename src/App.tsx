9bb7f77cc2b9dac8cbb607b6625ca8a5afe525ce
Jagan6923 <jeyaraman.saravanan23@gmail.com>
2024-12-23 10:45:52 +0530

    config page updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
-import LoginForm from "@components/LoginForm";
 import AppNavbar from "@components/Navbar";
 import "./App.scss";
-import ColorPaletteShowcase from "@components/Colors";
+import SignInOptions from "./pages/SignInOptions";
+import FinalSetup from "./pages/FinalSetup";
+import { ReusableStepper } from "./ui/molecules/StepperComponent";
 
 function App() {
+  const steps = [
+    { label: "Sign-In Options", component: SignInOptions, disabled: false },
+    { label: "Final Setup", component: FinalSetup, disabled: false },
+  ];
   return (
     <div className="App">
       <AppNavbar />
-      <LoginForm />
+      {/* <LoginForm /> */}
+      <ReusableStepper steps={steps} />
     </div>
   );
 }
