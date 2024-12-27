4467946e55494e60fc115e1adf1c3434549f2dd2
Kajalakshmi11 <146505088+Kajalakshmi11@users.noreply.github.com>
2024-12-27 03:47:26 -0800

    Scrum 6 create signin page (#11)
    * navbar added
    
    * navbar added
    
    * navbar added and sentry implemented successfully
    
    * google analytics added
    
    * navbar file moved to components
    
    * a,label,input atoms created
    
    * constants created for storing ga code and sentry dsn values
    
    * sentry moved to components
    
    * issues solved
    
    * atoms file structure changed, FormAction molecule created
    
    * role label component created
    
    * alies used, colors.scss created and variables used, rolelabel props changed to color
    
    * avatar component created
    
    * stepper component added
    
    * Toast Message added
    
    * signin page design modifiedd
    
    * integrate api
    
    * resolved merge conflicts
    
    * Updated
    
    ---------
    
    Co-authored-by: Jagan6923 <jeyaraman.saravanan23@gmail.com>
    Co-authored-by: Mentor Bridge <mentorbridgeindia@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,18 @@
 import AppNavbar from "@components/Navbar";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import "./App.scss";
-import FinalSetup from "./pages/FinalSetup";
-import { ReusableStepper } from "./ui/molecules/StepperComponent";
-import SignUpOptions from "./pages/SignUpOptions";
 import SocialLogin from "./components/SocialLogin";
 import ErrorBoundary from "./ErrorBoundary";
+import { Login } from "./pages/Login";
 
 function App() {
-  const steps = [
-    { label: "Sign-up Options", component: SignUpOptions, disabled: false },
-    { label: "Final Setup", component: FinalSetup, disabled: false },
-  ];
   return (
     <div className="App">
+      <AppNavbar />
+      <Login />
+      <ToastContainer />
       <ErrorBoundary>
-        <AppNavbar />
         <SocialLogin />
       </ErrorBoundary>
     </div>
