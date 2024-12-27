aa223c6f1deee063acd1778a1ad3dac198dafd9b
Mentor <mentorbridgeindia@gmail.com>
2024-12-27 16:43:13 +0530

    Updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,12 @@
 import AppNavbar from "@components/Navbar";
-import "./App.scss";
-import { Login } from "./pages/Login";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import FinalSetup from "./pages/FinalSetup";
-import { ReusableStepper } from "./ui/molecules/StepperComponent";
-import SignUpOptions from "./pages/SignUpOptions";
+import "./App.scss";
 import SocialLogin from "./components/SocialLogin";
 import ErrorBoundary from "./ErrorBoundary";
+import { Login } from "./pages/Login";
 
 function App() {
-  const steps = [
-    { label: "Sign-up Options", component: SignUpOptions, disabled: false },
-    { label: "Final Setup", component: FinalSetup, disabled: false },
-  ];
   return (
     <div className="App">
       <AppNavbar />
