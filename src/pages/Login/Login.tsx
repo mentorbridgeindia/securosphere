4d22e1e860d5e51ef48679a8492e284dd423b78e
Jagan6923 <jeyaraman.saravanan23@gmail.com>
2025-02-02 10:07:40 +0530

    client register login routing implemented

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -29,41 +29,8 @@ export const Login = () => {
     sessionStorage.removeItem("accessToken");
   }, []);
 
-  if (isValidClient && data) {
-    return (
-      <div className="d-flex align-items-center justify-content-center">
-        <Spinner isLoading={isPending} />
-        <Row className="w-100 center">
-          <Col lg={7} className="d-none d-lg-flex justify-content-center align-items-center">
-            <img src="https://cdn.vectorstock.com/i/500p/82/47/man-with-login-form-young-guy-enter-vector-54318247.jpg" alt="cover" className="img-fluid" />
-          </Col>
-          <Col lg={5} xs={12} md={10} className="d-flex align-items-center justify-content-center" style={{ height: "100vh", paddingBottom: "150px" }}>
-            <Card className="p-4 shadow rounded w-100">
-            <div className="d-flex justify-content-center brand-lg">
-                <IconLogo />
-              </div>
-              <h5>Welcome to {data.applicationName} login page</h5>
-              <SocialLoginButtons
-                isGoogleAvailable={data.socialProviders.google}
-                isLinkedinAvailable={data.socialProviders.linkedIn}
-                isGithubAvailable={data.socialProviders.github}
-                isFacebookAvailable={data.socialProviders.facebook}
-                isMicrosoftAvailable={data.socialProviders.microsoft}
-              />
-              <div className="mt-3 px-5">
-                <hr />
-              </div>
-              <div className="mt-3">
-                <LoginForm loginUser={loginUser} />
-              </div>
-            </Card>
-          </Col>
-        </Row>
-      </div>
-    );
-  }
-
-  return (
+  // Common JSX layout for the login page
+  const loginPageContent = (
     <div className="d-flex align-items-center justify-content-center">
       <Spinner isLoading={isPending} />
       <Row className="w-100 center">
@@ -75,11 +42,15 @@ export const Login = () => {
             <div className="d-flex justify-content-center brand-lg">
               <IconLogo />
             </div>
-            <h5 className="text-center mb-4">Welcome to SecuroSphere login page</h5>
+            <h5 className="text-center mb-4">
+              {isValidClient && data ? `Welcome to ${data.applicationName} login page` : "Welcome to SecuroSphere login page"}
+            </h5>
             <SocialLoginButtons
-              isGoogleAvailable
-              isLinkedinAvailable
-              isGithubAvailable
+              isGoogleAvailable={isValidClient && data ? data.socialProviders.google : true}
+              isLinkedinAvailable={isValidClient && data ? data.socialProviders.linkedIn : true}
+              isGithubAvailable={isValidClient && data ? data.socialProviders.github : true}
+              isFacebookAvailable={isValidClient && data ? data.socialProviders.facebook : false}
+              isMicrosoftAvailable={isValidClient && data ? data.socialProviders.microsoft : false}
             />
             <div className="mt-3 px-5">
               <hr />
@@ -92,4 +63,7 @@ export const Login = () => {
       </Row>
     </div>
   );
+
+  // Render the login page content
+  return loginPageContent;
 };
--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -10,6 +10,7 @@ import Row from "react-bootstrap/Row";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import { useInit } from "@hooks/useInit";
+
 export const Register = () => {
   const navigate = useNavigate();
   const { mutate: registerUser, isPending } = useRegister({
@@ -17,7 +18,7 @@ export const Register = () => {
       toast.success("Registration successful");
       navigate("/login");
     },
-    onError: (error) => {
+    onError: () => {
       toast.error("Registration failed! Something went wrong");
     },
   });
@@ -28,60 +29,52 @@ export const Register = () => {
     sessionStorage.removeItem("accessToken");
   }, []);
 
-  if (isValidClient && data) {
-    return (
-      <div className="d-flex align-items-center justify-content-center my-2 mt-5">
-        <Spinner isLoading={isPending} />
-        <Row className="w-100 center">
-          <Col lg={7} className="d-none d-lg-flex justify-content-center align-items-center">
-            <img src="https://cdn.vectorstock.com/i/500p/82/47/man-with-login-form-young-guy-enter-vector-54318247.jpg" alt="cover" className="img-fluid" />
-          </Col>
-          <Col lg={5} xs={12} md={10} className="d-flex align-items-center justify-content-center">
-            <Card className="p-4 shadow rounded w-100">
-              <div className="d-flex justify-content-center brand-lg">
-                <IconLogo />
-              </div>
-              <h6 className="text-center">Welcome to {data.applicationName} register page</h6>
-              <SocialLoginButtons
-                isGoogleAvailable={data.socialProviders.google}
-                isLinkedinAvailable={data.socialProviders.linkedIn}
-                isGithubAvailable={data.socialProviders.github}
-                isFacebookAvailable={data.socialProviders.facebook}
-                isMicrosoftAvailable={data.socialProviders.microsoft}
-              />
-              <div className="mt-3 px-5">
-                <hr />
-              </div>
-              <div className="mt-3">
-                <RegisterForm registerUser={registerUser} />
-              </div>
-              <a href={data.termsOfServiceUrl} target="_blank" rel="noopener noreferrer">
-                Terms of Service
-              </a>
-            </Card>
-          </Col>
-        </Row>
-      </div>
-    );
-  }
-
-  return (
+  // Common JSX layout for the register page
+  const registerPageContent = (
     <div className="d-flex align-items-center justify-content-center my-2 mt-5">
       <Spinner isLoading={isPending} />
       <Row className="w-100 center">
-        <Col lg={7} className="d-none d-lg-flex justify-content-center align-items-center">
-          <img src="https://cdn.vectorstock.com/i/500p/82/47/man-with-login-form-young-guy-enter-vector-54318247.jpg" alt="cover" className="img-fluid" />
+        <Col
+          lg={7}
+          className="d-none d-lg-flex justify-content-center align-items-center"
+        >
+          <img
+            src="https://cdn.vectorstock.com/i/500p/82/47/man-with-login-form-young-guy-enter-vector-54318247.jpg"
+            alt="cover"
+            className="img-fluid"
+          />
         </Col>
-        <Col lg={5} xs={12} md={10} className="d-flex align-items-center justify-content-center">
+        <Col
+          lg={5}
+          xs={12}
+          md={10}
+          className="d-flex align-items-center justify-content-center"
+        >
           <Card className="p-4 shadow rounded w-100">
             <div className="d-flex justify-content-center brand-lg">
               <IconLogo />
             </div>
-            <h5 className="text-center mb-4">Welcome to SecuroSphere register page</h5>
+            <h5 className="text-center mb-4">
+              {isValidClient && data
+                ? `Welcome to ${data.applicationName} register page`
+                : "Welcome to SecuroSphere register page"}
+            </h5>
             <SocialLoginButtons
-              isGoogleAvailable
-              isLinkedinAvailable
-              isGithubAvailable
+              isGoogleAvailable={
+                isValidClient && data ? data.socialProviders.google : true
+              }
+              isLinkedinAvailable={
+                isValidClient && data ? data.socialProviders.linkedIn : true
+              }
+              isGithubAvailable={
+                isValidClient && data ? data.socialProviders.github : true
+              }
+              isFacebookAvailable={
+                isValidClient && data ? data.socialProviders.facebook : false
+              }
+              isMicrosoftAvailable={
+                isValidClient && data ? data.socialProviders.microsoft : false
+              }
             />
             <div className="mt-3 px-5">
               <hr />
@@ -89,9 +82,21 @@ export const Register = () => {
             <div className="mt-3">
               <RegisterForm registerUser={registerUser} />
             </div>
+            {isValidClient && data && (
+              <a
+                href={data.termsOfServiceUrl}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Terms of Service
+              </a>
+            )}
           </Card>
         </Col>
       </Row>
     </div>
   );
+
+  // Render the register page content
+  return registerPageContent;
 };
