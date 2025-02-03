9c73542b76e249f704aadabfd148297567f7e66b
Mentor <mentorbridgeindia@gmail.com>
2025-02-03 21:02:16 +0530

    updated API url

--- src/api/api.ts
+++ src/api/api.ts
@@ -6,7 +6,7 @@ const token = sessionStorage.getItem("accessToken") ?? null;
 
 const clientId = sessionStorage.getItem("clientId");
 
-const isMainHost = window.location.hostname.startsWith("securosphere.in");
+const isMainHost = window.location.hostname.startsWith("www.securosphere.in");
 
 const subDomain = window.location.hostname.split(".")[0];
 
@@ -14,13 +14,13 @@ const isLocalHost = window.location.hostname.includes("localhost");
 
 let baseURL = isMainHost
   ? "http://api.securosphere.in"
-  : `http://${subDomain}.securosphere.in`;
+  : `http://${subDomain}.api.securosphere.in`;
 
 if (isLocalHost) {
   if (subDomain === "" || subDomain === "localhost") {
-    baseURL = "http://localhost:8080";
+    baseURL = "http://api.localhost:8080";
   } else {
-    baseURL = `http://${subDomain}.localhost:8080`;
+    baseURL = `http://${subDomain}.api.localhost:8080`;
   }
 }
 
