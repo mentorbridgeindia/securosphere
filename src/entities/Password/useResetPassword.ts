256ffc27529a0d0538b32d28f47b4edc0d9ec831
Mentor <mentorbridgeindia@gmail.com>
2025-02-15 22:34:17 +0530

    Modified endpoints

--- src/entities/Password/useResetPassword.ts
+++ src/entities/Password/useResetPassword.ts
@@ -1,10 +1,17 @@
 import { IMutationParams, MutationResponse } from "@/api/types";
 import { useMutation } from "@tanstack/react-query";
-import { RESET_PASSWORD_ENDPOINT, sendData } from "../../api";
+import {
+  RESET_PASSWORD_ENDPOINT,
+  THIRD_PARTY_RESET_PASSWORD_ENDPOINT,
+  sendData,
+} from "../../api";
 import { IResetPasswordMutation } from "./Password.types";
 
 const resetPassword = async (data: IResetPasswordMutation) => {
-  const response = await sendData(RESET_PASSWORD_ENDPOINT, data);
+  const url = sessionStorage.getItem("clientId")
+    ? THIRD_PARTY_RESET_PASSWORD_ENDPOINT
+    : RESET_PASSWORD_ENDPOINT;
+  const response = await sendData(url, data);
   return response;
 };
 
