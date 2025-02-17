a8cb7008ae198d4eb64b4ecc1cd5872cb4f28880
JEYASHREESELVAN <jeyashreeselvan9@gmail.com>
2025-02-17 23:06:02 +0530

    recap

---
+++ src/pages/Login/Login.tsx
@@ -0,0 +1,97 @@
+import { ReactComponent as IconLogo } from "@assets/icons/logo.svg";
+import { Spinner } from "@atoms/Spinner";
+import { ILoginMutation, useLogin } from "@entities/Login";
+import { LoginForm } from "@modules/LoginForm";
+import { SocialLoginButtons } from "@modules/SocialLogin";
+import { useEffect } from "react";
+import { Card } from "react-bootstrap";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import { GoogleReCaptchaProvider, useGoogleReCaptcha } from 'react-google-recaptcha-v3';
+import React, { useState } from "react";
+
+export const Login = () => {
+  const navigate = useNavigate();
+  const { executeRecaptcha } = useGoogleReCaptcha();
+  const { mutate: loginUser, isPending } = useLogin({
+    onSuccess: () => {
+      toast.success("Login successful! Welcome back!");
+      navigate("/");
+    },
+    onError: (error) => {
+      toast.error("Login failed. Please check your credentials.");
+    },
+  });
+
+  useEffect(() => {
+    sessionStorage.removeItem("accessToken");
+  }, []);
+   const handleLogin = async (formValues: ILoginMutation) => {
+      try {
+        if (!executeRecaptcha) {
+          throw new Error("reCAPTCHA not available");
+        }
+  
+        const token = await executeRecaptcha('login');
+        console.log('reCAPTCHA token:', token);
+  
+        loginUser(formValues);
+      } catch (error) {
+        toast.error("Registration verification failed");
+        console.error('Error:', error);
+      }
+    };
+  
+
+  return (
+    <div className="d-flex align-items-center justify-content-center">
+      <Spinner isLoading={isPending} />
+      <Row className="w-100 center">
+        <Col
+          lg={7}
+          className="d-none d-lg-flex justify-content-center align-items-center"
+        >
+          <img
+            src="https://cdn.vectorstock.com/i/500p/82/47/man-with-login-form-young-guy-enter-vector-54318247.jpg"
+            alt="cover"
+            className="img-fluid"
+          />
+        </Col>
+        <Col
+          lg={5}
+          xs={12}
+          md={10}
+          className="d-flex align-items-center justify-content-center"
+          style={{ height: "100vh", paddingBottom: "150px" }}
+        >
+          <Card className="p-4 shadow rounded w-100">
+            <div className="d-flex justify-content-center brand-lg">
+              <IconLogo />
+            </div>
+            <SocialLoginButtons
+              isGoogleAvailable
+              isLinkedinAvailable
+              isGithubAvailable
+            />
+            <div className="mt-3 px-5">
+              <hr />
+            </div>
+            <div className="mt-3">
+              <LoginForm loginUser={loginUser} />
+            </div>
+            <div className="mt-3">
+                        <GoogleReCaptchaProvider
+                  reCaptchaKey="6LcGntkqAAAAAIBxjVl7MxCIirQlvERk3Y1IQf6q"
+                >
+                          <LoginForm loginUser={handleLogin} />
+                      
+                </GoogleReCaptchaProvider>
+                </div> 
+          </Card>
+        </Col>
+      </Row>
+    </div>
+  );
+};
