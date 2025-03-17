3407b8a706b2fe7d1be644a863960c747abd830e
JEYASHREESELVAN <144306786+JEYASHREESELVAN@users.noreply.github.com>
2025-03-17 22:43:42 +0530

    Recaptcha (#33)
    * initial commit
    
    * cokies banner

--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -3,26 +3,62 @@ import { useInit } from "@entities/Domain";
 import { AuthCard } from "@modules/AuthCard";
 import { RegisterForm } from "@modules/RegisterForm";
 import { useEffect } from "react";
+import { toast } from "react-toastify";
+import { GoogleReCaptchaProvider, useGoogleReCaptcha } from 'react-google-recaptcha-v3';
+import React, {  useState } from "react";
+import { registerUser } from "@/entities/Register/register";
+import { IRegisterMutation } from "@/entities/Register";
 
 export const Register = () => {
-  const { data, isLoading } = useInit();
+  const { data, isLoading, } = useInit();
+  const { executeRecaptcha } = useGoogleReCaptcha();
+
 
   useEffect(() => {
     if (!isLoading && !data) {
       window.location.href = "https://securosphere.in";
     }
+
+    
     sessionStorage.removeItem("accessToken");
   }, [isLoading, data]);
+  const handleRegister = async (formValues: IRegisterMutation) => {
+        try {
+          if (!executeRecaptcha) {
+            throw new Error("reCAPTCHA not available");
+          }
+    
+          const token = await executeRecaptcha('register');
+          console.log('reCAPTCHA token:', token);
+    
+          registerUser(formValues);
+        } catch (error) {
+          toast.error("Registration verification failed");
+          console.error('Error:', error);
+        }
+      };
 
   if (isLoading) return <Spinner isLoading />;
-
+   
   return (
     <AuthCard
       imageUrl={
         "https://cdn.vectorstock.com/i/500p/82/47/man-with-login-form-young-guy-enter-vector-54318247.jpg"
       }
     >
+       <Spinner isLoading={isLoading} /> 
+      <div className="mt-3">
+        <GoogleReCaptchaProvider
+          reCaptchaKey="6LcGntkqAAAAAIBxjVl7MxCIirQlvERk3Y1IQf6q"
+          scriptProps={{
+            async: true,
+            defer: true,
+            appendTo: 'head',
+          }}
+        >
       <RegisterForm termsOfServiceUrl={data?.termsOfServiceUrl ?? ""} />
+      </GoogleReCaptchaProvider>
+      </div>
     </AuthCard>
   );
 };
